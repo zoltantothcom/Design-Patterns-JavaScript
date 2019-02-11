bf0068929482da373930580543b32323e222b3df
Zoltan Toth <contact@zoltantoth.com>
2019-02-10 20:02:50 -0500

    Removed knobs from Title story

--- stories/Title.stories.js
+++ stories/Title.stories.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, select, text } from '@storybook/addon-knobs';
 import { Title } from '../src/components/Title';
 
-const themes = ['dark', 'light'];
-
 storiesOf('Title', module)
-  .addDecorator(withKnobs)
-
   .add('dark theme', () => <Title mode="dark" text="Dark Theme" />)
-  .add('light theme', () => <Title mode="light" text="Light Theme" />)
-  .add('playground', () => (
-    <Title mode={select('mode', themes)} text={text('text', 'JavaScript Patterns')} />
-  ));
+  .add('light theme', () => <Title mode="light" text="Light Theme" />);
