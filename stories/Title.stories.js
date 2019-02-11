aedd49e56769ceea88c064b84fe4174acc9e8fdf
Zoltan Toth <contact@zoltantoth.com>
2019-02-10 20:06:24 -0500

    Removed props from Title in the story - component doesn't have them

--- stories/Title.stories.js
+++ stories/Title.stories.js
@@ -2,6 +2,4 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Title } from '../src/components/Title';
 
-storiesOf('Title', module)
-  .add('dark theme', () => <Title mode="dark" text="Dark Theme" />)
-  .add('light theme', () => <Title mode="light" text="Light Theme" />);
+storiesOf('Title', module).add('light theme', () => <Title />);
