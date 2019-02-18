2c98624b2489f048284a8c159e2d992c5388e1c5
Zoltan Toth <contact@zoltantoth.com>
2019-02-18 08:33:20 -0500

    ThemesProvider for stories made global in .storybook/config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,14 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withThemesProvider } from 'storybook-addon-styled-component-theme';
+import { themeCommon } from '../src/styles/themes/theme.common';
+import { themeLight } from '../src/styles/themes/theme.light';
+import { themeDark } from '../src/styles/themes/theme.dark';
+
+const light = { ...themeCommon, ...themeLight };
+const dark = { ...themeCommon, ...themeDark };
+
+const themes = [light, dark];
+addDecorator(withThemesProvider(themes));
 
 const req = require.context('../stories', true, /.stories.js$/);
 
--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -4,21 +4,11 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import Button from '../src/components/Button';
 import store from '../src/store';
-import { withThemesProvider } from 'storybook-addon-styled-component-theme';
-import { themeCommon } from '../src/styles/themes/theme.common';
-import { themeLight } from '../src/styles/themes/theme.light';
-import { themeDark } from '../src/styles/themes/theme.dark';
-
-const light = { ...themeCommon, ...themeLight };
-const dark = { ...themeCommon, ...themeDark };
-
-const themes = [light, dark];
 
 const withProvider = story => <Provider store={store}>{story()}</Provider>;
 
 storiesOf('Button', module)
   .addDecorator(withProvider)
-  .addDecorator(withThemesProvider(themes))
   .addDecorator(withKnobs)
 
   .add('label', () => (
