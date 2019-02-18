74fb5182ed4b642e829cf6a7fff912380de34d29
Zoltan Toth <contact@zoltantoth.com>
2019-02-18 10:17:59 -0500

    Results component styled

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { restart } from '../actions';
+import Button from './Button';
+
+const Percentage = styled.h1`
+  border: 4px solid ${props => props.level};
+  border-radius: 50%;
+  color: ${props => props.level};
+  font-size: 4rem;
+  height: 10rem;
+  margin: auto;
+  line-height: 10rem;
+  text-align: center;
+  width: 10rem;
+`;
+
+const Info = styled.p`
+  color: ${props => props.theme.buttonBorderHover};
+  margin: 3rem 0;
+  text-align: center;
+`;
+
+const Restart = styled.div`
+  margin: 3rem 4rem;
+  text-align: center;
+`;
 
 const Results = ({ answers, onClick }) => {
   let correct = 0;
@@ -9,14 +34,30 @@ const Results = ({ answers, onClick }) => {
 
   answers.map(answer => (answer.correct ? correct++ : wrong++));
 
+  const percent = Math.ceil((correct * 100) / 23);
+  let level = 'red';
+
+  switch (true) {
+  case percent >= 80:
+    level = 'green';
+    break;
+  case percent >= 60:
+    level = 'gold';
+    break;
+  case percent >= 40:
+    level = 'orange';
+    break;
+  }
+
   return (
     <div>
-      <h2>Results</h2>
-      <h1>{Math.ceil((correct * 100) / 23)}%</h1>
-      You've got <strong>{correct}</strong> patterns right out of {answers.length}.
-      <div>
-        <button onClick={onClick}>Try Again</button>
-      </div>
+      <Info>
+        You've got <strong>{correct}</strong> patterns right out of {answers.length}.
+      </Info>
+      <Percentage level={level}>{percent}%</Percentage>
+      <Restart>
+        <Button label="Try Again" id="try_again" onClick={onClick} />
+      </Restart>
     </div>
   );
 };
