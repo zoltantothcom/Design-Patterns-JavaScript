e18361b994f62bb6eb3b94b363eadf10011dadb0
Zoltan Toth <contact@zoltantoth.com>
2019-02-19 10:10:01 -0500

    About page content added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledAbout = styled.div`
+  color: ${props => props.theme.buttonBorderHover};
+
+  a {
+    border-bottom: 1px solid transparent;
+    color: ${props => props.theme.orange};
+    text-decoration: none;
+
+    &:hover {
+      border-bottom: 1px solid ${props => props.theme.orange};
+    }
+  }
+`;
+
+const Header = styled.h3`
+  color: ${props => props.theme.buttonColorHover};
+  margin-top: 2rem;
+`;
 
 const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
+  <StyledAbout>
+    <Header>The Game</Header>
+    <p>
+      JavaScript Design Patterns - to get familiar with the design patterns, test yourself (or
+      someone else) or simply for fun. Enjoy!
+    </p>
+
+    <Header>References</Header>
+    <p>
+      All the code samples impudently taken from this{' '}
+      <cite>
+        <a href="//github.com/fbeline/Design-Patterns-JS" target="_blank">
+          awesome code compilation
+        </a>{' '}
+        by Felipe Beline
+      </cite>{' '}
+      - because I have no shame.
+    </p>
+    <p>
+      If you want a deep dive into the subject I can't recommend enough{' '}
+      <cite>
+        <a href="//addyosmani.com/resources/essentialjsdesignpatterns/book/" target="_blank">
+          Learning JavaScript Design Patterns
+        </a>{' '}
+        by Addy Osmani
+      </cite>
+      .
+    </p>
+  </StyledAbout>
 );
 
 export default About;
