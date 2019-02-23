3b5a08b0ef5404280d9102dd260dee4dc5d223b4
Zoltan Toth <contact@zoltantoth.com>
2019-02-23 10:16:46 -0500

    Moved some wrappers up into index from Layout

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './src/store';
 import Layout from './src/Layout';
 
 const App = () => (
   <Provider store={store}>
-    <Layout />
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
   </Provider>
 );
 
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { connect } from 'react-redux';
 import styleLight from './styles/hljs/hljs.light';
@@ -12,6 +12,7 @@ import { themeLight } from './styles/themes/theme.light';
 import { themeDark } from './styles/themes/theme.dark';
 import Game from './pages/Game';
 import About from './pages/About';
+import { getMode } from './selectors';
 
 const Layout = props => {
   const { mode } = props;
@@ -25,17 +26,15 @@ const Layout = props => {
   }
 
   return (
-    <Router>
-      <ThemeProvider theme={theme}>
-        <React.Fragment>
-          <GlobalStyle mode={mode} />
-          <Header />
+    <ThemeProvider theme={theme}>
+      <React.Fragment>
+        <GlobalStyle mode={mode} />
+        <Header />
 
-          <Route exact path="/" render={props => <Game {...props} style={style} />} />
-          <Route path="/about" component={About} />
-        </React.Fragment>
-      </ThemeProvider>
-    </Router>
+        <Route exact path="/" render={() => <Game style={style} />} />
+        <Route path="/about" component={About} />
+      </React.Fragment>
+    </ThemeProvider>
   );
 };
 
@@ -43,6 +42,8 @@ Layout.propTypes = {
   mode: PropTypes.string.isRequired
 };
 
-const mapStateToProps = ({ mode }) => ({ mode });
-
-export default connect(mapStateToProps)(Layout);
+export default withRouter(
+  connect(state => ({
+    mode: getMode(state)
+  }))(Layout)
+);
