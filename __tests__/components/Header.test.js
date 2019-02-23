990d6b2cff40851f94178afa5e6d294eed1512c4
Zoltan Toth <contact@zoltantoth.com>
2019-02-23 13:59:18 -0500

    Tests for HEADER component added

--- __tests__/components/Header.test.js
+++ __tests__/components/Header.test.js
@@ -1,16 +1,51 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import configureMockStore from 'redux-mock-store';
+import { ThemeProvider } from 'styled-components';
+import { themeLight } from '../../src/styles/themes/theme.light';
+import { themeDark } from '../../src/styles/themes/theme.dark';
 import 'jest-styled-components';
 import Header from '../../src/components/Header';
 
-describe('<Header /> component', () => {
-  let header;
+const mockStore = configureMockStore();
+const store = mockStore({
+  js: 'es5',
+  mode: 'dark'
+});
 
-  beforeEach(() => {
-    header = shallow(<Header />);
+describe('<Header /> component', () => {
+  it('renders with LIGHT theme', () => {
+    const header = shallow(
+      <div key="unique">
+        <Router>
+          <Provider store={store}>
+            <ThemeProvider theme={themeLight}>
+              <Header />
+            </ThemeProvider>
+          </Provider>
+        </Router>
+      </div>
+    );
+    const tree = renderer.create(header).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 
-  it('renders without crashing', () => {
-    expect(header).toBeTruthy();
+  it('renders with DARK theme', () => {
+    const header = shallow(
+      <div key="unique">
+        <Router>
+          <Provider store={store}>
+            <ThemeProvider theme={themeDark}>
+              <Header />
+            </ThemeProvider>
+          </Provider>
+        </Router>
+      </div>
+    );
+    const tree = renderer.create(header).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
