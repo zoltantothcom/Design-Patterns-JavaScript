27329d6ba8d67e1834664caec2cf626554629766
Zoltan Toth <contact@zoltantoth.com>
2019-02-26 12:17:05 -0500

    Refactored global styles tests

--- __tests__/styles/global.test.js
+++ __tests__/styles/global.test.js
@@ -1,43 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import 'jest-styled-components';
-import { ThemeProvider } from 'styled-components';
 import { themeLight } from '../../src/styles/themes/theme.light';
 import { themeDark } from '../../src/styles/themes/theme.dark';
 import GlobalStyle from '../../src/styles/global';
 
 describe('Global style', () => {
   it('renders with a LIGHT theme', () => {
-    const tree = renderer
-      .create(
-        <ThemeProvider theme={themeLight}>
-          <body>
-            <GlobalStyle />
-            <div id="root">TEST</div>
-          </body>
-        </ThemeProvider>
-      )
-      .toTree();
-
-    expect(
-      tree.instance._reactInternalFiber.child.child.child.child.stateNode.styleSheet.target
-    ).toMatchSnapshot();
+    renderer.create(<GlobalStyle theme={themeLight} />);
+    expect(document.head).toMatchSnapshot();
   });
 
   it('renders with a DARK theme', () => {
-    const tree = renderer
-      .create(
-        <ThemeProvider theme={themeDark}>
-          <body>
-            <GlobalStyle />
-            <div id="root">TEST</div>
-          </body>
-        </ThemeProvider>
-      )
-      .toTree();
-
-    expect(
-      tree.instance._reactInternalFiber.child.child.child.child.stateNode.styleSheet.target
-    ).toMatchSnapshot();
+    renderer.create(<GlobalStyle theme={themeDark} />);
+    expect(document.head).toMatchSnapshot();
   });
 });
