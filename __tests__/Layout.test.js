4e51c294c1d4bd40242ea823a411b6366126c876
Zoltan Toth <contact@zoltantoth.com>
2019-02-26 15:52:54 -0500

    Tests for Layout added

--- __tests__/Layout.test.js
+++ __tests__/Layout.test.js
@@ -1,38 +1,113 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import ShallowRenderer from 'react-test-renderer/shallow';
-import { Route, Router, withRouter } from 'react-router-dom';
-import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
+import { ThemeProvider } from 'styled-components';
+import { themeLight } from '../src/styles/themes/theme.light';
+import { themeDark } from '../src/styles/themes/theme.dark';
+import { BrowserRouter } from 'react-router-dom';
 import Layout from '../src/Layout';
 
 const mockStore = configureMockStore();
-const store = mockStore({
-  mode: 'dark',
-  js: 'es5'
-});
+const patterns = [
+  {
+    uuid: 'abc123',
+    name: 'Prototype',
+    type: 'creational',
+    codeES5: 'Code ES5 - Prototype',
+    codeES6: 'Code ES6 - Prototype',
+    answered: false,
+    answerId: null,
+    correct: null
+  },
 
-it('renders without crashing', () => {
-  shallow(<Layout />);
-});
+  {
+    uuid: 'abc234',
+    name: 'SIngleton',
+    type: 'creational',
+    codeES5: 'Code ES5 - Singleton',
+    codeES6: 'Code ES6 - Singleton',
+    answered: false,
+    answerId: null,
+    correct: null
+  }
+];
 
-it('renders in DARK more', () => {
-  console.log(shallow(<Layout />).debug());
+describe('Layout', () => {
+  describe('Header - LIGHT mode', () => {
+    let tree;
+    const store = mockStore({
+      patterns: patterns,
+      progress: {
+        answers: [],
+        remaining: [patterns[1]],
+        current: patterns[0]
+      },
+      mode: 'light',
+      js: 'es5'
+    });
 
-  const renderer = new ShallowRenderer();
-  renderer.render(<Layout />);
-  const result = renderer.getRenderOutput();
+    beforeEach(() => {
+      tree = mount(
+        <Provider store={store}>
+          <BrowserRouter>
+            <ThemeProvider theme={themeLight}>
+              <Layout />
+            </ThemeProvider>
+          </BrowserRouter>
+        </Provider>
+      );
+    });
 
-  console.log(result.props.children);
+    it('has the correct title', () => {
+      expect(tree.find('Header').text()).toMatch('JavaScript Design Patterns');
+    });
 
-  // const tree = ShallowRenderer(
-  //   <Provider store={store}>
-  //     <Router>
-  //       <Layout />
-  //     </Router>
-  //   </Provider>
-  // ).toJSON();
-  // console.log(tree.debug());
-  // expect(tree).toMatchSnapshot();
+    it('renders 3 links', () => {
+      expect(tree.find('a')).toHaveLength(3);
+    });
+
+    it('renders 2 toggle buttons', () => {
+      expect(tree.find('Header button')).toHaveLength(2);
+    });
+  });
+
+  describe('Header - DARK mode', () => {
+    let tree;
+    const store = mockStore({
+      patterns: patterns,
+      progress: {
+        answers: [],
+        remaining: [patterns[1]],
+        current: patterns[0]
+      },
+      mode: 'dark',
+      js: 'es5'
+    });
+
+    beforeEach(() => {
+      tree = mount(
+        <Provider store={store}>
+          <BrowserRouter>
+            <ThemeProvider theme={themeDark}>
+              <Layout />
+            </ThemeProvider>
+          </BrowserRouter>
+        </Provider>
+      );
+    });
+
+    it('has the correct title', () => {
+      expect(tree.find('Header').text()).toMatch('JavaScript Design Patterns');
+    });
+
+    it('renders 3 links', () => {
+      expect(tree.find('a')).toHaveLength(3);
+    });
+
+    it('renders 2 toggle buttons', () => {
+      expect(tree.find('Header button')).toHaveLength(2);
+    });
+  });
 });
