be5d3e487f594008ad99c0e873f45f69ef782966
Zoltan Toth <contact@zoltantoth.com>
2019-02-28 07:03:03 -0500

    Correctly calculating the score for Twitter button

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { restart } from '../actions/restart';
-import { getCurrent } from '../selectors';
+import { getCurrent, getAnswers } from '../selectors';
 import ButtonContainer from '../components/ButtonContainer';
 import ProgressBar from '../components/ProgressBar';
 import Code from '../components/Code';
@@ -40,51 +40,61 @@ const ShareContainer = styled.p`
   text-align: center;
 `;
 
-const Game = ({ current, style, onRestart }) => (
-  <Fragment>
-    {current ? (
-      <Fragment>
-        <ProgressBar />
-        <Code style={style} />
-        <ButtonContainer />
-      </Fragment>
-    ) : (
-      <Fragment>
-        <Result />
-        <Percentage />
-        <Restart>
-          <Button label="Try Again" id="try_again" onClick={onRestart} />
-        </Restart>
-        <ShareContainer>
-          <TwitterButton
-            className="twitter-share-button"
-            data-dnt="true"
-            href="https://twitter.com/intent/tweet?text=I%20scored%2021%20out%20of%2023%20JavaScript%20Design%20Patterns!&url=https://javascript-design-patterns.netlify.com"
-          >
-            Tweet Your Score
-          </TwitterButton>
+const Game = ({ current, answers, style, onRestart }) => {
+  let correct;
 
-          <GitHubButton
-            href="https://github.com/zoltantothcom/JavaScript-Design-Patterns"
-            aria-label="Star zoltantothcom/JavaScript-Design-Patterns on GitHub"
-          >
-            Star on GitHub
-          </GitHubButton>
-        </ShareContainer>
-      </Fragment>
-    )}
-  </Fragment>
-);
+  if (!current) {
+    correct = answers.filter(item => item.correct).length;
+  }
+
+  return (
+    <Fragment>
+      {current ? (
+        <Fragment>
+          <ProgressBar />
+          <Code style={style} />
+          <ButtonContainer />
+        </Fragment>
+      ) : (
+        <Fragment>
+          <Result />
+          <Percentage />
+          <Restart>
+            <Button label="Try Again" id="try_again" onClick={onRestart} />
+          </Restart>
+          <ShareContainer>
+            <TwitterButton
+              className="twitter-share-button"
+              data-dnt="true"
+              href={`https://twitter.com/intent/tweet?text=I%20scored%20${correct}%20out%20of%2023%20in%20JavaScript%20Design%20Patterns%20game!%20Try%20it!&url=https://javascript-design-patterns.netlify.com`}
+            >
+              Tweet Your Score
+            </TwitterButton>
+
+            <GitHubButton
+              href="https://github.com/zoltantothcom/JavaScript-Design-Patterns"
+              aria-label="Star zoltantothcom/JavaScript-Design-Patterns on GitHub"
+            >
+              Star on GitHub
+            </GitHubButton>
+          </ShareContainer>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
 
 Game.propTypes = {
   style: PropTypes.object.isRequired,
   onRestart: PropTypes.func.isRequired,
+  answers: PropTypes.array.isRequired,
   current: PropTypes.object
 };
 
 export default connect(
   state => ({
-    current: getCurrent(state)
+    current: getCurrent(state),
+    answers: getAnswers(state)
   }),
   {
     onRestart: () => restart()
