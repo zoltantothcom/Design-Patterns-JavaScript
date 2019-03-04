3356d35dc20ebceecbc3fb23b61fe96f166f78a3
Zoltan Toth <contact@zoltantoth.com>
2019-03-04 12:43:52 -0500

    Percentage story fix

--- stories/Percentage.stories.js
+++ stories/Percentage.stories.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Percentage from '../src/components/Percentage';
+import { Percentage } from '../src/components/Percentage';
+
+const initialArray = (n, isAnswered, isCorrect) => {
+  let arr = Array(...Array(n));
+
+  return arr.map((x, i) => {
+    return {
+      patternId: null,
+      answerId: null,
+      answered: isAnswered,
+      correct: isCorrect,
+      uuid: i
+    };
+  });
+};
+
+const answers0 = initialArray(23, true, false);
+const answers50 = [...initialArray(11, true, false), ...initialArray(12, true, true)];
+const answers100 = initialArray(23, true, true);
 
 storiesOf('Percentage', module)
-  .add('under 40', () => <Percentage percent={30} />)
-  .add('between 40 and 70', () => <Percentage percent={60} />)
-  .add('over 70', () => <Percentage percent={90} />);
+  .add('under 40', () => <Percentage answers={answers0} />)
+  .add('between 40 and 70', () => <Percentage answers={answers50} />)
+  .add('over 70', () => <Percentage answers={answers100} />);
