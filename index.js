41e292555d776cc9cddab4634e490784589c46de
Zoltan Toth <contact@zoltantoth.com>
2019-03-08 08:56:29 -0500

    Add scroll to top on page change

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from './src/store';
 import Layout from './src/Layout';
+import ScrollToTop from './src/components/ScrollToTop';
 
 const App = () => (
   <Provider store={store}>
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <Router>
+      <ScrollToTop>
+        <Layout />
+      </ScrollToTop>
+    </Router>
   </Provider>
 );
 
