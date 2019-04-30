883609773ef36c7baddd7c98519d99e62c7d2f48
ixnv <autechregescom@gmail.com>
2019-04-30 05:11:42 +0300

    Fix coverage

---
+++ __tests__/localStorage.js
@@ -0,0 +1,28 @@
+import {
+  loadState,
+  saveState
+} from '../src/helpers/localStorage';
+
+describe('LocalStorage utilities', () => {
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
+
+  const data = {
+    foo: 1
+  };
+
+  it('should save and load state', () => {
+    saveState(data);
+
+    const state = loadState();
+    expect(state).toMatchObject(data);
+  });
+
+  it('should not throw exceptions if localStorage is undefined', () => {
+    delete window.localStorage;
+
+    expect(saveState(data)).toBeUndefined();
+    expect(loadState()).toBeUndefined();
+  });
+});
